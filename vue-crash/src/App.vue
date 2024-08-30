e5c53bed48f77eecdbd215c9c20349dec7abf0ec
Maksim Senin <wecpo@yandex.ru>
2024-08-30 13:50:57 +0300

    feat: init project

---
+++ vue-crash/src/App.vue
@@ -0,0 +1,23 @@
+<script>
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: `123`
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+</template>
+
+<style>
+h1 {
+  color: red;
+}
+</style>
