0512fb618d02d2fed83c1e651148bed87f060d28
Senin Maksim <wecpo@yandex.ru>
2024-09-02 14:32:01 +0300

    feat: added Hero, HomeCards, JobListing, jobs.json

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <script setup>
+import Hero from './components/Hero.vue';
+import HomeCards from './components/HomeCards.vue';
+import JobListing from './components/JobListings.vue';
 import Navbar from './components/Navbar.vue';
 
 </script>
@@ -6,4 +9,7 @@ import Navbar from './components/Navbar.vue';
 <template>
   <h1 class="text-2xl">Vue Jobs</h1>
   <Navbar />
+  <Hero />
+  <HomeCards />
+  <JobListing />
 </template>
